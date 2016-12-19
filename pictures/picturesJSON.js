e4658bee15966e0f3e983169e2851923adddf148
mdnyhus <mdnyhus@gmail.com>
2016-12-19 18:42:16 -0500

    add favicon and initial picture files that are used

---
+++ pictures/picturesJSON.js
@@ -0,0 +1,468 @@
+var picturesJSON = {
+  "pictures" : [
+		{
+			"name" : "Salt Marsh",
+			"folder" : "SaltMarsh",
+			"location" : "Facebook HQ",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "SaltMarsh1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "SaltMarsh2.jpeg",
+					"title" : "Details",
+					"description" : "Increase the structure to give the image more definition"
+				},
+				{
+					"name" : "SaltMarsh3.jpeg",
+					"title" : "Tune Image (Colours)",
+					"description" : "Increase the saturation and contrast to add interest; the marsh is too bright, but that will be fixed later"
+				},
+				{
+					"name" : "SaltMarsh4.jpeg",
+					"title" : "Tune Image (Light)",
+					"description" : "Add contrast with the lighting to further add variation"
+				},
+				{
+					"name" : "SaltMarsh5.jpeg",
+					"title" : "White Balance",
+					"description" : "Make the water selectively bluer, and add a slight blue tinge throughout"
+				},
+				{
+					"name" : "SaltMarsh6.jpeg",
+					"title" : "Details",
+					"description" : "Add more structure to the image to increase the definition"
+				},
+				{
+					"name" : "SaltMarsh7.jpeg",
+					"title" : "Tune Image (Selective)",
+					"description" : "Selectively desaturate the image; the marsh was too bright and the salt should look very white, and add more contrast in the sky"
+				},
+			]
+		},
+		{
+			"name" : "Cracked Ice",
+			"folder" : "CrackedIce",
+			"location" : "Yosemite",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "CrackedIce1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "CrackedIce2.jpeg",
+					"title" : "Details (Initial)",
+					"description" : "Sharpen and add some initial structure so that image tuning has some variation to work with"
+				},
+				{
+					"name" : "CrackedIce3.jpeg",
+					"title" : "Tune Image (Initial)",
+					"description" : "Bump the saturation and contrast up to add interest, and change the lighting to add even more contrast"
+				},
+				{
+					"name" : "CrackedIce4.jpeg",
+					"title" : "Details (Touch Up)",
+					"description" : "Increase the structre and sharpness again to further add variation"
+				},
+				{
+					"name" : "CrackedIce5.jpeg",
+					"title" : "Tune Image (Repeated)",
+					"description" : "The picture is still a little grey, so repeat the initial tune to really add colour"
+				},
+				{
+					"name" : "CrackedIce6.jpeg",
+					"title" : "Tune Image (Touch Up)",
+					"description" : "The next step will be to tint the colour of the image, so to prepare darken the image slightly so the really reflective pieces stand out, and add colour variation once again"
+				},
+				{
+					"name" : "CrackedIce7.jpeg",
+					"title" : "White balance",
+					"description" : "Add some green into the image for interest"
+				},
+			]
+		},
+		{
+			"name" : "SF MOMA",
+			"folder" : "SFMOMA",
+			"location" : "San Fransisco MOMA",
+			"description" : "Another minimalist picture attempt; this is looking up the side of the Muserum of Modern Art",
+			"stages" : [
+				{
+					"name" : "SFMOMA1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "SFMOMA2.jpeg",
+					"title" : "Details",
+					"description" : "Increase the structure to give the image more definition"
+				},
+				{
+					"name" : "SFMOMA3.jpeg",
+					"title" : "Tune Image (Colours)",
+					"description" : "Increase the saturation and contrast for add more colour variation"
+				},
+				{
+					"name" : "SFMOMA4.jpeg",
+					"title" : "Tune Image (Light)",
+					"description" : "Increase the lighting contrast with less shadows and more highlights, and wash out the image a bit"
+				},
+				{
+					"name" : "SFMOMA5.jpeg",
+					"title" : "White Balance",
+					"description" : "Tint the image to inject some interesting colour and add a candy-esque look"
+				},
+			]
+		},
+		{
+			"name" : "Grey Gate",
+			"folder" : "GreyGate",
+			"location" : "Golden Gate Bridge",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "GreyGate1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "GreyGate2.jpeg",
+					"title" : "Details",
+					"description" : "Add some sharpness and structure to give the image more definition"
+				},
+				{
+					"name" : "GreyGate3.jpeg",
+					"title" : "Tune Image (Colour)",
+					"description" : "Flatten the colour of the sky and water and bring out the effect of the cloud; also desaturate the image slightly"
+				},
+				{
+					"name" : "GreyGate4.jpeg",
+					"title" : "Tune Image (Light)",
+					"description" : "Darken the image"
+				},
+				{
+					"name" : "GreyGate5.jpeg",
+					"title" : "Selective (Bridge)",
+					"description" : "Selectively colour the bridge to bring back the red"
+				},
+				{
+					"name" : "GreyGate6.jpeg",
+					"title" : "Tune Image (Desaturate)",
+					"description" : "Selectively desaturate and darken the water to take away any remaining colour"
+				},
+			]
+		},
+		{
+			"name" : "Fall Colours",
+			"folder" : "FallColours",
+			"location" : "University of Washington",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "FallColours1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "FallColours2.jpeg",
+					"title" : "Details",
+					"description" : "Add a little initial sharpness and structure to give the image more variation, so that the next few steps have something to work with"
+				},
+				{
+					"name" : "FallColours3.jpeg",
+					"title" : "Tune Image (Colour)",
+					"description" : "Increase the sautration and contrast to give the image more colour and interest"
+				},
+				{
+					"name" : "FallColours4.jpeg",
+					"title" : "Tune Image (Light)",
+					"description" : "Increase the difference between highlights and shadows to add more contrast with light"
+				},
+				{
+					"name" : "FallColours5.jpeg",
+					"title" : "Details (Selective)",
+					"description" : "Selectively sharpen just the in-focus section of the trunk.  The blurred section should stay out of focus, and should realy contrast the in-focus trunk"
+				},
+				{
+					"name" : "FallColours6.jpeg",
+					"title" : "Lens Blur",
+					"description" : "My cell phone camera used an effect to blur everything far away from the lens, but it was poorly applied.  This just blends the out of focus and in-focus sections better with a transition"
+				},
+			]
+		},
+		{
+			"name" : "Escalator Lights",
+			"folder" : "EscalatorLights",
+			"location" : "Levi's Stadium",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "EscalatorLights1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "EscalatorLights2.jpeg",
+					"title" : "Tune Image (Light)",
+					"description" : "Before adding structure, I wanted to try and decrease the noise of the light bleeding past the LEDs"
+				},
+				{
+					"name" : "EscalatorLights3.jpeg",
+					"title" : "Details",
+					"description" : "Increase the structure to give the image more definition"
+				},
+				{
+					"name" : "EscalatorLights4.jpeg",
+					"title" : "Tune Image (Colours)",
+					"description" : "Enriched the colours of the image by increasing the saturation and contrast and also further isolated the LEDs"
+				},
+				{
+					"name" : "EscalatorLights5.jpeg",
+					"title" : "Tune Image (Bars)",
+					"description" : "After increasing the saturation, the bars below the elevator handles were oversaturated; I decreased the brightness and contrast to give that part more definition again"
+				},
+				{
+					"name" : "EscalatorLights6.jpeg",
+					"title" : "White Balance (Even Out)",
+					"description" : "In between the escalators, the LEDs had very patchy colours.  This light fix enriches the blue in the image and evens out the colours.  I liked the colour of the escalator, so I selected only the LEDs."
+				},
+				{
+					"name" : "EscalatorLights7.jpeg",
+					"title" : "White Balance (Green)",
+					"description" : "I wanted more colour variety in the image; parts of the LEDs already looked a bit green, so I tinted the i mage green on a gradient (so the top is still blue) but again omitted the escalator."
+				},
+				{
+					"name" : "EscalatorLights8.jpeg",
+					"title" : "Lens Blur",
+					"description" : "This was a horizontal lens blur from the top of the image to add depth, since the LEDs at the bottom are farther away.  I ommitted the escalator since it's in the foreground."
+				},
+			]
+		},
+		{
+			"name" : "Buckyball",
+			"folder" : "Buckyball",
+			"location" : "Exploratorium",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "Buckyball1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "Buckyball2.jpeg",
+					"title" : "Details",
+					"description" : "Increase the structure to give the image more definition"
+				},
+				{
+					"name" : "Buckyball3.jpeg",
+					"title" : "Tune Image (Colours)",
+					"description" : "Increase the saturation to improve the colour"
+				},
+				{
+					"name" : "Buckyball4.jpeg",
+					"title" : "Tune Image (Light)",
+					"description" : "The central ball was too bright, so selectively darken it"
+				},
+				{
+					"name" : "Buckyball5.jpeg",
+					"title" : "Tune Image (Soften)",
+					"description" : "Some pink lights are over saturated and bleeding out past the sculpture, so selectively darken those parts"
+				},
+				{
+					"name" : "Buckyball6.jpeg",
+					"title" : "White Balance",
+					"description" : "Circle blur centered on the middle ball to bring it into more focus"
+				},
+			]
+		},
+		{
+			"name" : "Golden Gate",
+			"folder" : "GoldenGate",
+			"location" : "Golden Gate Bridge",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "GoldenGate1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "GoldenGate2.jpeg",
+					"title" : "Healing",
+					"description": "Remove the cable lines in the corners on the left side"
+				},
+				{
+					"name" : "GoldenGate3.jpeg",
+					"title" : "Details (Not Sky)",
+					"description" : "For this image, I worked on the sky and everything else separately.  First, selectively add detail to the bridge, land and water to add more definition"
+				},
+				{
+					"name" : "GoldenGate4.jpeg",
+					"title" : "Tune Image (Not Sky)",
+					"description" : "Selectively add saturation and contrast to the bridge, land and water to increase the colour variation"
+				},
+				{
+					"name" : "GoldenGate5.jpeg",
+					"title" : "Details (Sky)",
+					"description" : "Now to work on the sky; there are some parts of the sky that are very pixelated, so decrease the structure to smooth out the sky before tuning it"
+				},
+				{
+					"name" : "GoldenGate6.jpeg",
+					"title" : "Tune Image (Sky)",
+					"description" : "Add interest into the sky by increasing the saturation and contrast to add back some of the structure of the clouds and a blue tinge"
+				},
+				{
+					"name" : "GoldenGate7.jpeg",
+					"title" : "Tune Image (Touch Up)",
+					"description" : "Increase the saturation and contrast of the entire image except for the bridge to add some final touch ups"
+				},
+			]
+		},
+		{
+			"name" : "Icy Wheat",
+			"folder" : "IcyWheat",
+			"location" : "Yosemite",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "IcyWheat1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "IcyWheat2.jpeg",
+					"title" : "Details",
+					"description": "Increase the structure slightly to give the image some initial definition.  Since the background is blurry, I don't want to increase the structure of the whole image too much."
+				},
+				{
+					"name" : "IcyWheat3.jpeg",
+					"title" : "Tune Image (Colour)",
+					"description" : "Increase the saturation and contrast to improve the colours of the image"
+				},
+				{
+					"name" : "IcyWheat4.jpeg",
+					"title" : "Tune Image (Light)",
+					"description" : "Dim the image slightly; the background of the image was becoming over-exposed"
+				},
+				{
+					"name" : "IcyWheat5.jpeg",
+					"title" : "Details (Just Wheat)",
+					"description" : "Since the grain of wheat is the only thing in focus, selectively increase just its structure and sharpness"
+				},
+				{
+					"name" : "IcyWheat6.jpeg",
+					"title" : "White Balance",
+					"description" : "The image was too blue, especialy the ground in the foreground.  Increase the temperature slightly to add more orange tones, mostly to the ground but also throughout"
+				},
+			]
+		},
+		{
+			"name" : "Sagrada Familia",
+			"folder" : "SagradaFamilia",
+			"location" : "Sagrada Familia",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "SagradaFamilia1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "SagradaFamilia2.jpeg",
+					"title" : "Details",
+					"description": "Increase the structure to give the image more definition; add more definition to the pillar than the background since it's in focus"
+				},
+				{
+					"name" : "SagradaFamilia3.jpeg",
+					"title" : "Tune Image (Colour)",
+					"description" : "Increase the saturation and contrast to improve the colours of the image"
+				},
+				{
+					"name" : "SagradaFamilia4.jpeg",
+					"title" : "Tune Image (Light)",
+					"description" : "Increase the highlights/shadows contrast so that the light on the pillar stands out more"
+				},
+				{
+					"name" : "SagradaFamilia5.jpeg",
+					"title" : "Lens Blur",
+					"description" : "To really make the pillar stand out, I blurred the background"
+				},
+				{
+					"name" : "SagradaFamilia6.jpeg",
+					"title" : "Tune Image (Touch Up)",
+					"description" : "A final touch up; increase the saturation and contrast, as well as the lighting contrast, to make the colours on the pillar more vibrant"
+				},
+			]
+		},
+		{
+			"name" : "White Cliffs",
+			"folder" : "WhiteCliffs",
+			"location" : "White Cliffs of Dover",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "WhiteCliffs1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "WhiteCliffs2.jpeg",
+					"title" : "Details",
+					"description": "Increase the structure to give the image more definition"
+				},
+				{
+					"name" : "WhiteCliffs3.jpeg",
+					"title" : "Tune Image (Colour)",
+					"description" : "Increase the saturation and contrast to improve the colours of the image"
+				},
+				{
+					"name" : "WhiteCliffs4.jpeg",
+					"title" : "Tune Image (Light)",
+					"description" : "Increase the highlights/shadows contrast to make the lighting stand out more"
+				},
+				{
+					"name" : "WhiteCliffs5.jpeg",
+					"title" : "White Balance",
+					"description" : "Increasing the saturation made the image a little orange; counteract this effect by decreasing the temperature"
+				},
+			]
+		},
+		{
+			"name" : "El Capitan",
+			"folder" : "ElCapitan",
+			"location" : "Yosemite",
+			"description" : "",
+			"stages" : [
+				{
+					"name" : "ElCapitan1.jpeg",
+					"title" : "Original"
+				},
+				{
+					"name" : "ElCapitan2.jpeg",
+					"title" : "Details",
+					"description": "Increase the structure to give the image more definition"
+				},
+				{
+					"name" : "ElCapitan3.jpeg",
+					"title" : "Tune Image (Colour)",
+					"description" : "Increase the saturation and contrast to improve the colours of the image"
+				},
+				{
+					"name" : "ElCapitan4.jpeg",
+					"title" : "Tune Image (Light)",
+					"description" : "Increase the highlights/shadows contrast to make the lighting stand out more"
+				},
+				{
+					"name" : "ElCapitan5.jpeg",
+					"title" : "Details (Ground)",
+					"description" : "Selectively add more definition to the ground and mountain face"
+				},
+				{
+					"name" : "ElCapitan6.jpeg",
+					"title" : "White Balance",
+					"description" : "Increasing the saturation made the image more orange; decrease the temperature to counteract this"
+				},
+				{
+					"name" : "ElCapitan7.jpeg",
+					"title" : "White Balance (Sky)",
+					"description" : "Selectively increase the temperature in gradients in the sky to add more colour variation"
+				},
+			]
+		},
+  ]
+}
